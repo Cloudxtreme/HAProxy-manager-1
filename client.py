0e469004604599e44e00cbc7b5f6e50acc035f6a
119Vik <vitalij_ua@ukr.net>
2013-08-07 15:01:35 +0300

    client accept server address

--- client.py
+++ client.py
@@ -1,18 +1,23 @@
 """
     This client do many test calls to test server
 """
+import sys
 import urllib
 
+_BASE_URL= "http://{}/{}"
+_BASE_IP = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1:9000'
+
+print _BASE_IP
 
 def run_get():
     """
         Get queries for Load balancer test
     """
-    BASE_URL = "http://127.0.0.1:9000/{}"
+    
     x = 0
     for number in xrange(10000):
         try:
-            resp = urllib.urlopen(BASE_URL.format(number))
+            resp = urllib.urlopen(_BASE_URL.format(_BASE_IP, number))
             server_info = resp.read()
             print "Request #{} was processed by {}".format(number, server_info)
         except :
