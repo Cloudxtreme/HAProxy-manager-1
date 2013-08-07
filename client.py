4b02a494d2bcf897f238a97bb005177957294cb9
119Vik <vitalij_ua@ukr.net>
2013-08-07 16:39:15 +0300

    update for test client

--- client.py
+++ client.py
@@ -1,28 +1,51 @@
 """
     This client do many test calls to test server
 """
+import gevent 
+from gevent import monkey; monkey.patch_socket()
 import sys
 import urllib
 
+import getopt
+
+
+argv = sys.argv[1:]
+try:
+    args, stuff = getopt.getopt(argv, "hs:c:")
+    params =  dict(args)
+except:
+    params = dict()
+
+
 _BASE_URL= "http://{}/{}"
-_BASE_IP = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1:9000'
+_BASE_IP = params.get('-s') or '127.0.0.1:9000'
+
+CALLS_RANGE = params.get('-c') or 10000
 
 print _BASE_IP
+
+def call(num):
+    """ 
+    """
+    try:
+        resp = urllib.urlopen(_BASE_URL.format(_BASE_IP, num))
+        server_info = resp.read()
+        print "Request #{} was processed by {}".format(number, server_info)
+    except:
+        return 1
+
+    return 0
 
 def run_get():
     """
         Get queries for Load balancer test
     """
     
-    x = 0
-    for number in xrange(10000):
-        try:
-            resp = urllib.urlopen(_BASE_URL.format(_BASE_IP, number))
-            server_info = resp.read()
-            print "Request #{} was processed by {}".format(number, server_info)
-        except :
-            x += 1
-
+    jobs = [gevent.spawn(call, num) for num in xrange(CALLS_RANGE)]
+    gevent.joinall(jobs)
+    x = sum([job.value for job in jobs])
+    # jobs = [call(num) for num in xrange(CALLS_RANGE)]
+    # x = sum([job for job in jobs])
     print "{} Packets lost".format(x)
 
 run_get()
